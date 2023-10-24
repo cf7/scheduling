3919d1ab68621652043d7da79fc81a2c03189a9a
c <c@cs-macbook-air.lan>
2023-10-23 23:35:11 -0700

    flatten dirs

--- src/api/API.py
+++ src/api/API.py
@@ -21,18 +21,19 @@ they can be provided separately to each route method
 """
 
 
+from typing import Dict
 from api.routes.Routes import HTTP_Method
 
 
 class API:
-    def __init__(self, route_factory):
+    def __init__(self, route_factory: Dict[str, Dict[str, callable]]):
         self.route_factory = route_factory
 
     @classmethod
     def get_instance(cls, route_factory):
         return cls(route_factory)
 
-    def get_handler(self, http_method: HTTP_Method, resource_path: str):
+    def get_handler(self, http_method: HTTP_Method, resource_path: str) -> callable:
         handlers_by_method = self.route_factory.get(http_method)
         return handlers_by_method.get(resource_path)
 
--- src/persistence/MembersRepository/query.py
+++ src/persistence/MembersRepository/query.py
@@ -9,4 +9,4 @@ class MembersQuery:
 
     @staticmethod
     def get_members(member_ids: List[int]) -> List[Member]:
-        return Member(id=1, name="test user", schedule_id=1, slot_ids=[])
+        return [Member(id=1, name="test user", schedule_id=1, slot_ids=[])]
